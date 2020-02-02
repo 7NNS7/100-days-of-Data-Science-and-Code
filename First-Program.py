9dfc9166d61422bd9832b58fd99728e47d17990c
Nithin <nithin@getambee.com>
2020-02-02 22:54:53 +0530

    Added first program using git

---
+++ First-Program.py
@@ -0,0 +1,7 @@
+'''
+This is the first program in 100 days of github challenge.
+Everything starts with zero.
+Here is my first step.
+'''
+
+print('Hello World. I'm in for the 100 days github challenge.')
